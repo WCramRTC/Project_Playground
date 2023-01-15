f30b473a9c33bb35cde7186ecc216871d7c25fec
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-01-14 22:44:06 -0800

    Initial commit

---
+++ BlogPost.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace Project_Playground
+{
+    public class BlogPost
+    {
+        // Fields
+        string _header;
+        string _body;
+        Brush _headerColor;
+        Brush _bodyColor;
+        DateTime _blogPostTime;
+
+        // Constructor
+        public BlogPost(string header, string body)
+        {
+            _header = header;
+            _body = body;
+            _blogPostTime = DateTime.Now;
+        } // BlogPost
+
+        public BlogPost(string header, string body, Brush headerColor, Brush bodyColor)
+        {
+            _header = header;
+            _body = body;
+            _headerColor = headerColor;
+            _bodyColor = bodyColor;
+            _blogPostTime = DateTime.Now;
+        } // BlogPost
+
+        // Properties
+        public string Header
+        {
+            get => _header;
+            set
+            {
+                if(value != "" && value.Length > 3)
+                {
+                    _header = value;
+                }
+                else
+                {
+                    MessageBox.Show("Enter a valid header");
+                }
+            }
+        } // Header
+
+        public string Body
+        {
+            get => _body;
+            set
+            {
+                if (value != "" && value.Length > 10)
+                {
+                    _body = value;
+                }
+                else
+                {
+                    MessageBox.Show("Enter a valid body");
+                }
+            }
+        } // Header
+
+        // Methods
+
+        public Paragraph Post()
+        {
+            Paragraph p = new Paragraph();
+            p.Inlines.Add(HeaderFormat());
+            p.Inlines.Add(BodyFormat());
+            return p;
+        }
+        // Oppertunity to show method overloads
+        private Run HeaderFormat()
+        {
+            string spacing = "\n\n";
+            Run header = new Run($"{_blogPostTime.ToLocalTime()} - {_header}{spacing}");
+            header.Foreground = _headerColor;
+            header.FontSize = 22;
+            header.FontWeight = FontWeights.Bold;
+            return header;
+        }
+
+        private Run BodyFormat()
+        {
+            Run body = new Run(_body);
+            body.Foreground = _bodyColor;
+            return body;
+        }
+
+        public override string ToString()
+        {
+            return $"{_blogPostTime.ToShortDateString()} - {_header}";
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_Playground
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<BlogPost> blogPosts = new ObservableCollection<BlogPost>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbBlogPosts.ItemsSource = blogPosts;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string header = txtHeader.Text;
+            string body = rtbBody.Text;
+            MessageBox.Show(btnHeaderColor.Foreground.ToString());
+
+
+            BlogPost blogPost = new BlogPost(header, body, btnHeaderColor.Foreground, btnBodyColor.Foreground);
+
+            blogPosts.Add(blogPost);
+
+        }
+
+        private void ClearBlogColumn()
+        {
+            fdDisplay.Blocks.Clear();
+        }
+
+        private void lbBlogPosts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ClearBlogColumn();
+            int selectedIndex = lbBlogPosts.SelectedIndex;
+
+            fdDisplay.Blocks.Add(blogPosts[selectedIndex].Post());
+        }
+
+        private void btnGenColor_Click(object sender, RoutedEventArgs e)
+        {
+            string r = txtR.Text;
+            string g = txtG.Text;
+            string b = txtB.Text;
+
+            btnGenColor.Foreground = GenerateColor(r, g, b);
+        }
+
+        private void btnHeaderColor_Click(object sender, RoutedEventArgs e)
+        {
+            btnHeaderColor.Foreground = btnGenColor.Foreground;
+        }
+
+        private void btnBodyColor_Click(object sender, RoutedEventArgs e)
+        {
+            btnBodyColor.Foreground = btnGenColor.Foreground;
+        }
+
+        public Brush GenerateColor(string red, string green, string blue)
+        {
+            byte r = CheckColor(red);
+            byte g = CheckColor(green);
+            byte b = CheckColor(blue);
+
+            return new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+
+        public byte CheckColor(string color)
+        {
+            byte rgb = 0;
+            bool isANumber = byte.TryParse(color, out rgb);
+            bool isBetween0And25 = rgb < 0 || rgb > 255;
+
+            if (!isANumber && !isBetween0And25)
+            {
+                MessageBox.Show($"{color} needs to be a number between 0 and 255");
+            }
+
+            return rgb;
+
+        }
+
+    }
+}
